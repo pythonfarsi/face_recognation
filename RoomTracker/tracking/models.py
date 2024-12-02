2a52e60d08e1e3559f2cd05fd93d140c018e1516
mohsen <zamin5555@gmail.com>
2024-12-03 00:13:55 +0330

    add person model to project

--- RoomTracker/tracking/models.py
+++ RoomTracker/tracking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    face_id=models.CharField(max_lenght=50, unique=True)
+    image=models.ImageField(upload_to='faces/')
+    first_seen=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.face_id
